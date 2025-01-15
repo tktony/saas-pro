72f35e9a5ceee5fa72b426b8d9d1e9043621cdd9
Temurbek <161694620+tktony@users.noreply.github.com>
2025-01-15 19:33:06 +0100

    settings.py DATABASE_URL configurations
    Signed-off-by: Temurbek <161694620+tktony@users.noreply.github.com>

--- src/tonhome/settings.py
+++ src/tonhome/settings.py
@@ -82,7 +82,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "tonhome.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -93,6 +92,18 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30) # 30 seconds
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=CONN_MAX_AGE,
+            conn_health_checks=True,
+    ) 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
