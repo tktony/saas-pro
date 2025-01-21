791fd2ae2f5d86076fe75b90c5e47a2e36d028d8
Temurbek <161694620+tktony@users.noreply.github.com>
2025-01-21 19:49:02 +0100

    Updated Build Args
    Signed-off-by: Temurbek <161694620+tktony@users.noreply.github.com>

--- src/tonhome/settings.py
+++ src/tonhome/settings.py
@@ -26,8 +26,6 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-print("DEBUG", DEBUG, type(DEBUG))
-
 ALLOWED_HOSTS = [
     ".railway.app" # something like https://saas.prod.railway.app
 ]
@@ -94,7 +92,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30) # 30 seconds
-DATABASE_URL = config("DATABASE_URL", cast=str)
+DATABASE_URL = config("DATABASE_URL", default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
