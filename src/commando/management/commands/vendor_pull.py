c8bf43cdfc6dcf2c5b51dcec15356a449ec2d645
Temurbek <161694620+tktony@users.noreply.github.com>
2025-01-21 21:12:31 +0100

    whitenoise configuration and .map js issue fixed
    Signed-off-by: Temurbek <161694620+tktony@users.noreply.github.com>

--- src/commando/management/commands/vendor_pull.py
+++ src/commando/management/commands/vendor_pull.py
@@ -3,9 +3,9 @@ import helpers
 from typing import Any
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
+# Directory where vendor static files are stored
 STATICFILES_VENDOR_DIR = getattr(settings, 'STATICFILES_VENDOR_DIR')
-
+# URLs to download vendor files
 VENDOR_STATICFILES = {
     "flowbite.min.css":"https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
     "flowbite.min.js":"https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",
--- src/tonhome/settings.py
+++ src/tonhome/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -151,6 +152,15 @@ STATICFILES_DIRS = [
 # output for python manage.py collectstatic
 # local cdn
 STATIC_ROOT = BASE_DIR / "local-cdn"
+
+# < Django 4.2
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# for support on django 4.2+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
