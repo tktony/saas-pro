458cdee1d110607d8e10db7489e94d8ae8ca0450
Temurbek <161694620+tktony@users.noreply.github.com>
2025-02-09 20:45:31 +0100

    Login with GitHub via Django AllAuth
    Signed-off-by: Temurbek <161694620+tktony@users.noreply.github.com>

--- src/tonhome/settings.py
+++ src/tonhome/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
     'widget_tweaks',
     'slippers',
 ]
@@ -173,7 +174,11 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 # Provider specific settings
-SOCIALACCOUNT_PROVIDERS = { }
+SOCIALACCOUNT_PROVIDERS = { 
+    "github": {
+        "VERIFIED_EMAIL": True
+    }
+}
 
 LANGUAGE_CODE = "en-us"
 
