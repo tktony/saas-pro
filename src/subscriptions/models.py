1ba361da43721e48246b2db1e984aa3fbc53b675
Temurbek <161694620+tktony@users.noreply.github.com>
2025-02-16 22:16:10 +0100

    Our Django Subscription Model as a Stripe Product
    Signed-off-by: Temurbek <161694620+tktony@users.noreply.github.com>

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,3 +1,4 @@
+import helpers.billing
 from django.db import models
 from django.contrib.auth.models import Group, Permission
 from django.db.models.signals import post_save
@@ -16,13 +17,20 @@ SUBSCRIPTION_PERMISSIONS = [
 
 # Create your models here.
 class Subscription(models.Model):
+    """
+    Subscription Plan = Stripe Product
+    """
     name = models.CharField(max_length=120)
     active = models.BooleanField(default=True)
     groups = models.ManyToManyField(Group) # one-to-one
-    permissions =  models.ManyToManyField(Permission, limit_choices_to={
-        "content_type__app_label": "subscriptions", "codename__in": [x[0]for x in SUBSCRIPTION_PERMISSIONS]
+    permissions =  models.ManyToManyField(Permission, 
+    limit_choices_to={
+        "content_type__app_label": "subscriptions", 
+        "codename__in": [x[0]for x in SUBSCRIPTION_PERMISSIONS]
         }
     )
+
+    stripe_id = models.CharField(max_length=120, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -30,6 +38,17 @@ class Subscription(models.Model):
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
 
+    def save(self, *args, **kwargs):
+        if not self.stripe_id:
+            stripe_id = helpers.billing.create_product(
+                    name=self.name, 
+                    metadata={
+                        "subscription_plan_id": self.id
+                    }, 
+                    raw=False
+                )
+            self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
 
 class UserSubscription(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # when the user is deleted their subscription is deleted as well
