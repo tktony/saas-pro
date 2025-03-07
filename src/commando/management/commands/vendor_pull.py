8167b2cfc5a7a3179ac17037d5f3584b09d89eef
Temurbek <161694620+tktony@users.noreply.github.com>
2025-03-07 01:29:44 +0100

    Using the SaaS theme to Fix Missing Tailwind Classes
    Signed-off-by: Temurbek <161694620+tktony@users.noreply.github.com>

--- src/commando/management/commands/vendor_pull.py
+++ src/commando/management/commands/vendor_pull.py
@@ -7,6 +7,7 @@ from django.core.management.base import BaseCommand
 STATICFILES_VENDOR_DIR = getattr(settings, 'STATICFILES_VENDOR_DIR')
 # URLs to download vendor files
 VENDOR_STATICFILES = {
+    "saas-theme.min.css": "https://raw.githubusercontent.com/codingforentrepreneurs/SaaS-Foundations/main/src/staticfiles/theme/saas-theme.min.css",
     "flowbite.min.css":"https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
     "flowbite.min.js":"https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",
     "flowbite.min.js.map": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js.map"
